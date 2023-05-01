42dd8a2bc901a029be9823183dcb895f8416c95a
the1dynasty <the1dynasty@gmail.com>
2023-05-01 13:09:05 -0400

    Added support for .webp images

--- appfilter_script.py
+++ appfilter_script.py
@@ -9,7 +9,12 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 FOLDER_PATH = SCRIPT_DIR
 
 # Get the list of PNG files in the folder
+#PNG_FILES = [os.path.join(FOLDER_PATH, f) for f in os.listdir(FOLDER_PATH) if f.endswith(".png")]
+
+# Get the list of PNG and WebP files in the folder
 PNG_FILES = [os.path.join(FOLDER_PATH, f) for f in os.listdir(FOLDER_PATH) if f.endswith(".png")]
+WEBP_FILES = [os.path.join(FOLDER_PATH, f) for f in os.listdir(FOLDER_PATH) if f.endswith(".webp")]
+IMAGE_FILES = PNG_FILES + WEBP_FILES
 
 # Create a new file for storing the modified appfilter.xml
 MODIFIED_FILE = os.path.join(FOLDER_PATH, "appfilter_modified.xml")
@@ -23,7 +28,8 @@ with open(os.path.join(FOLDER_PATH, "appfilter.xml"), "r") as input_file, open(M
             # Extract the drawable name from the line
             drawable_name = line.split('drawable="')[1].split('"')[0]
             # Check if the drawable name is in the list of PNG files
-            if os.path.join(FOLDER_PATH, drawable_name + '.png') in PNG_FILES:
+            #if os.path.join(FOLDER_PATH, drawable_name + '.png') in PNG_FILES:
+            if os.path.join(FOLDER_PATH, drawable_name + '.png') in IMAGE_FILES or os.path.join(FOLDER_PATH, drawable_name + '.webp') in IMAGE_FILES:
                 # If there is a matching drawable name, add the line to the list of drawable lines
                 drawables.append(line)
                 continue
@@ -43,9 +49,3 @@ with open(os.path.join(FOLDER_PATH, "appfilter_organized.xml"), "w") as output_f
     for drawable in drawables:
         output_file.write('    ' + drawable.strip() + '\n')
     output_file.write('</resources>\n')
-
-# Remove the modified file
-os.remove(MODIFIED_FILE)
-
-# Display a message to the user indicating that the script has finished
-print("The script has finished generating the 'appfilter_organized.xml' file.")
